ba91c7ec915860e568f10104c0669e7a9bf6c472
chaibeechew <chaibeechew@gmail.com>
2026-09-06 14:53:15 +0800

    feat(brand): enforce canonical lotus across core chrome

--- app/template.js
+++ app/template.js
@@ -3,6 +3,7 @@ import "./laneriq-launch-splash.css";
 import "./auth/auth-living-intelligence.css";
 import "./auth/auth-lotus-brand-override.css";
 import "./living-intelligence-refresh.css";
+import "./laneriq-brand-lock.css";
 import LaneriqLaunchSplash from "./components/LaneriqLaunchSplash";
 
 export default function Template({ children }) {
