43fe83ddd9ce2d53b62e7a27e4e9fa6f61c05c3a
chaibeechew <chaibeechew@gmail.com>
2026-09-06 15:05:26 +0800

    Restore account security menu contract label

--- app/components/AccountNav.js
+++ app/components/AccountNav.js
@@ -123,7 +123,7 @@ export default function AccountNav() {
       <button type="button" role="menuitem" onClick={() => go("/my-apps")}>▣ My Projects</button>
       <button type="button" role="menuitem" onClick={() => go("/account/device-compute")}>◎ Device &amp; Compute</button>
       <button type="button" role="menuitem" onClick={() => go("/account/cloud")}>◌ LANERIQ Cloud</button>
-      <button type="button" role="menuitem" onClick={() => go("/account/security")}>◇ Security &amp; Email</button>
+      <button type="button" role="menuitem" onClick={() => go("/account/security")}>◇ Security & Email</button>
       <button type="button" role="menuitem" onClick={() => go("/studio")}>▦ Studio</button>
       <button type="button" role="menuitem" onClick={() => go("/community-chat")}>◫ Community</button>
       <button className="signout" type="button" role="menuitem" onClick={signOut} disabled={signingOut}>Sign out</button>
