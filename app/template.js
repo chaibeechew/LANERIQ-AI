422ba48c9b7ab5b82a67e6bdc221377dbc9c68ef
chaibeechew <chaibeechew@gmail.com>
2026-09-06 15:02:37 +0800

    Load unified lotus utility surface presentation

--- app/template.js
+++ app/template.js
@@ -4,6 +4,7 @@ import "./auth/auth-living-intelligence.css";
 import "./auth/auth-lotus-brand-override.css";
 import "./living-intelligence-refresh.css";
 import "./laneriq-brand-lock.css";
+import "./liui-lotus-surface-refresh.css";
 import LaneriqLaunchSplash from "./components/LaneriqLaunchSplash";
 
 export default function Template({ children }) {
